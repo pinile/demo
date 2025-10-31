49d6c3a823a3ccc5f4ec4fab8104aaeb6112ebeb
PGlitvinov <vtb4114175@corp.dev.vtb>
2025-10-31 15:43:36 +0300

    отключил логгирование в консоль для hikari

--- src/main/java/ru/stepchenkov/db/dao/DataSourceProvider.java
+++ src/main/java/ru/stepchenkov/db/dao/DataSourceProvider.java
@@ -1,9 +1,12 @@
 package ru.stepchenkov.db.dao;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import ru.stepchenkov.env.config.DBConfig;
 
 import javax.sql.DataSource;
@@ -14,8 +17,9 @@ public class DataSourceProvider {
     public static DataSource getH2DataSource(DBConfig config) {
         log.info("Создание H2 DataSource");
 
-        HikariConfig hikariConfig = new HikariConfig();
+        disableHikariLogging();
 
+        HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(config.h2JdbcUrl());
         hikariConfig.setUsername(config.user());
         hikariConfig.setPassword(config.password());
@@ -26,5 +30,17 @@ public class DataSourceProvider {
         log.info("H2 DataSource создана");
 
         return new HikariDataSource(hikariConfig);
+    }
+
+    private static void disableHikariLogging() {
+        try {
+            // Отключаем через Logback
+            LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+            loggerContext.getLogger("com.zaxxer.hikari").setLevel(Level.OFF);
+
+            // Отключаем JUL
+            java.util.logging.LogManager.getLogManager().reset();
+            java.util.logging.Logger.getLogger("com.zaxxer.hikari").setLevel(java.util.logging.Level.OFF);
+        } catch (Exception e) {}
     }
 }
